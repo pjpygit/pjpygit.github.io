ebe9d91ef3299bb2b94f1a13052ee23df1474b91
pjpygit <100914189+pjpygit@users.noreply.github.com>
2022-03-06 22:45:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ app = Flask(__name__)
 app.secret_key = 'very secret many random bytes'
 
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login_p')
+def login_p():
+    return render_template('login_p.html')
+
+@app.route('/reg_p')
+def reg_p():
+    return render_template('reg_p.html')
+
 
 if __name__ == "__main__":
     app.run()
