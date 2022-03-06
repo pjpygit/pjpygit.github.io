dccb13a7bad7e8f0202d8e66d25d51a0329f9b1e
pjpygit <100914189+pjpygit@users.noreply.github.com>
2022-03-06 18:15:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, redirect, session, flash, jsonify, url_for
 
 app = Flask(__name__)
+app.secret_key = 'very secret many random bytes'
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
