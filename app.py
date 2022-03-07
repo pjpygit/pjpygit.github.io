cea40ae208fc4f1822e2cdbcec3d6a6e5f831002
pjpygit <100914189+pjpygit@users.noreply.github.com>
2022-03-07 10:25:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request, redirect, session, flash, jsonify, url_for
+from functools import wraps
+import re
+from werkzeug.utils import secure_filename
+import os
+from datetime import datetime
+import sqlite3
+import random, threading, webbrowser
+from routes import *
+from waitress import serve
+from sqlite3 import Error
+
 
 app = Flask(__name__)
+app.register_blueprint(routes)
 app.secret_key = 'very secret many random bytes'
 
 
